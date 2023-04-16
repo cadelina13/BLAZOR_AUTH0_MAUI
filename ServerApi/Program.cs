a5ba8cdf13900a5dacac89e16f4bb4c5d0f2d347
Rio Cadelina <rio.cadelina@mims.com>
2023-04-16 13:54:24 +0800

    added auth0 in the api

--- ServerApi/Program.cs
+++ ServerApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ServerApi;
 using System.Security.Claims;
 
@@ -19,7 +20,7 @@ builder.Services.AddAuthentication(options =>
 }).AddJwtBearer(options =>
 {
     options.Authority = "https://dev-auth0-maui-blazor.us.auth0.com/";
-    options.Audience = "https://localhost:5001";
+    options.Audience = "https://locahost:5001";
     options.TokenValidationParameters = new TokenValidationParameters
     {
         NameClaimType = ClaimTypes.NameIdentifier
@@ -32,6 +33,43 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "ServerAPI", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                Scopes = new Dictionary<string, string>
+                {
+                    { "openid", "Open Id" }
+                },
+                AuthorizationUrl = new Uri("https://dev-auth0-maui-blazor.us.auth0.com" + "authorize?audience=" + "https://localhost:5001")
+            }
+        }
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,7 +78,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+    c.OAuthClientId("jhqaFhLQz7TeHyZp5y6XGxZsI7Qb4Yty");
+});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
